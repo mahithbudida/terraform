841b85abc6d4c40a4efa9bb3bf16109255bbf3c7
mahithbudida <mahithbudida@outllook.com>
2021-07-24 07:58:37 +0530

    Hi

--- 02-Variables/var.tf
+++ 02-Variables/var.tf
@@ -6,6 +6,13 @@ output "sample" {
   value = "var.sample"
 }
 
-output "sample1" {
-  value = "${var.sample}"
+output "sample2" {
+  value = "${var.sample} - From Terraform"
+}
+
+// varibale if it is combained
+
+variable "input" {}
+output "output" {
+  value = var.input
 }
