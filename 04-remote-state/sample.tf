a6edcf8dc00e0f43f4d16b88413e101609ce4445
mahithbudida <mahithbudida@outllook.com>
2021-07-26 08:25:11 +0530

    Hi

--- 04-remote-state/sample.tf
+++ 04-remote-state/sample.tf
@@ -12,11 +12,11 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-terraform {
-  backend "s3"{
-    bucket   = "terraform-b57"
-    key      = "sample/terraform.tfstate"
-    region   = "us-east-1"
-  }
-}
+//terraform {
+ // backend "s3"{
+ //   bucket   = "terraform-b57"
+  //  key      = "sample/terraform.tfstate"
+//    region   = "us-east-1"
+ // }
+//}
 
