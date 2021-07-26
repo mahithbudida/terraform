c1215171f66eab8d450337d7c4bb983ad6650629
mahithbudida <mahithbudida@outllook.com>
2021-07-26 08:04:59 +0530

    Hi

---
+++ 04-remote-state/sample.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "sample" {
+  ami                    = "ami-074df373d6bafa625"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = "sg-061b8d4afe08bb944"
+
+  tags                   = {
+    Name                 = "Sample"
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+}
+
+terraform {
+  backend "s3"{
+    bucket   = "terraform-b57"
+    key      = "sample/terraform.tfstate"
+    region   = "us-east-1"
+  }
+}
+
