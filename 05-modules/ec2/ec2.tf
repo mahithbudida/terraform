4ce1a9a1c4752dfab587f2bc650dd3280668d3df
mahithbudida <mahithbudida@outllook.com>
2021-07-27 22:42:58 +0530

    Hi

---
+++ 05-modules/ec2/ec2.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "sample" {
+  count                   =  2
+  ami                     = "ami-074df373d6bafa625"
+  instance_type           = var.TYPE
+  vpc_security_group_ids  = [var.SG_ID]
+
+  tags                    = {
+    Name                  =   "Server-${count.index}"
+  }
+}
+
+variable "TYPE" {}
+variable "SG_ID" {}
+
+output "PRIVATE_IP" {
+  value = "aws_instance.sample.*.private_ip"
+}
---
+++ 05-modules/main.tf
@@ -0,0 +1,19 @@
+module "sg" {
+  source      = "./sg"
+}
+
+module "ec2" {
+  source      = "./ec2"
+  depends_on  = module.sg
+  SG_ID       = module.sg.SG_ID
+  TYPE        = "ts.small"
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+output "PRIVATE_IP" {
+  value = module.ec2.PRIVATE_IP
+}
