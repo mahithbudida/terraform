562e0a3583066ce4bd4414c5ffac983246e367d2
mahithbudida <mahithbudida@outllook.com>
2021-07-27 23:07:12 +0530

    Hi

---
+++ 06-conditions/sample.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "sample" {
+  ami            = "ami-074df373d6bafa625"
+  instance_type  = var.TYPE == NULL ? "t3.micro" : var.TYPE
+  tags           = {Name = "sample"}
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "TYPE" {
+  default = null
+}
